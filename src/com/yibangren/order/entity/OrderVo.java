097342e8f8f0d5329ccba5c17e6d6a92fcaf1019
彭松 <pinePeng@outlook.com>
2017-03-15 09:59:30 +0800

    新建中间表现层entity

---
+++ src/com/yibangren/order/entity/OrderVo.java
@@ -0,0 +1,12 @@
+package com.yibangren.order.entity;
+
+import java.io.Serializable;
+
+public class OrderVo implements Serializable {
+
+    /**
+     * 实现Serializable接口实现序列化
+     */
+    private static final long serialVersionUID = -2778792121421971672L;
+
+}
